3ae4530d97c1ac5754031535ce62dd5cdb93b9dc
Kevin Engström <kevin.engstrom@hotmail.com>
2019-12-16 13:57:50 +0100

    Add let canvas and context

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,5 +1,8 @@
 console.log("Loaded file");
 
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2D");
+
 function WriteBoard() {
   console.log("WriteBoard() function reached");
 }
