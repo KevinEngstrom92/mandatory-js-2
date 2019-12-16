80655c33df8b5d68138ce16bea0f036e041b1f71
Kevin Engström <kevin.engstrom@hotmail.com>
2019-12-16 14:13:35 +0100

    Add textweight initial layout finished

--- TicTacToe.js
+++ TicTacToe.js
@@ -5,6 +5,15 @@ let context = canvas.getContext("2D");
 
 function WriteBoard() {
   console.log("WriteBoard() function reached");
+  var player = document.getElementById("current-player");
+
+  if (player.textContent === "Blue") {
+    player.setAttribute("style", "color: blue; font-weight: bold;");
+  } else {
+    player.setAttribute("style", "color: red; font-weight: bold;");
+  }
+
+  //  MainGameLoop();
 }
 
 function MainGameLoop() {}
