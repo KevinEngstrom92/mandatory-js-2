1e3cef1715590a9ad45afd306c3f2dc1557056a7
Kevin Engstrom <kevin.engstrom@hotmail.com>
2019-12-18 08:49:19 +0100

    Changes made locally at home, remove canvas in favor for table, logic handles not clicking same td twice

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,13 +1,15 @@
 console.log("Loaded file");
 
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2D");
+document.getElementById("table").addEventListener("click", makeMove);
 
 var mappen = new Array(3).fill(new Array(3)).fill("");
+
+
 
 function WriteBoard() {
   console.log("WriteBoard() function reached");
   var player = document.getElementById("current-player");
+
 
   if (player.textContent === "Blue") {
     player.setAttribute("style", "color: blue; font-weight: bold;");
@@ -15,7 +17,36 @@ function WriteBoard() {
     player.setAttribute("style", "color: red; font-weight: bold;");
   }
 
+
   //  MainGameLoop();
 }
+
+
+
+function makeMove(event)
+{
+    var player = document.getElementById("current-player");
+  console.log("click");
+
+ let sign = "";
+
+
+
+if(event.target.textContent === "X" || event.target.textContent === "O")
+{
+  //Do nothing, since the cell was already taken by any player
+}
+else
+{
+  player.textContent === "Blue" ? sign = "X" : sign = "O";
+ player.textContent === "Blue" ? player.textContent = "Red" : player.textContent = "Blue";
+ event.target.textContent = sign;
+}
+
+
+
+ WriteBoard();
+}
+
 
 function MainGameLoop() {}
