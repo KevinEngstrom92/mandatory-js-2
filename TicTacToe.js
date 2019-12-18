aefed469c77390dd9569937b250372d123f1ba11
Kevin Engstrom <kevin.engstrom@hotmail.com>
2019-12-18 09:58:20 +0100

    Add modal and logic to check if no moves remain

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,10 +1,23 @@
 console.log("Loaded file");
 
+var modalClose = document.getElementById("close");
+modalClose.addEventListener("click", close);
 document.getElementById("table").addEventListener("click", makeMove);
 
 var mappen = new Array(3).fill(new Array(3)).fill("");
 
+var table = document.getElementById("table");
 
+
+for (var i = 0, row; row = table.rows[i]; i++) {
+   //iterate through rows
+   //rows would be accessed using the "row" variable assigned in the for loop
+   for (var j = 0, col; col = row.cells[j]; j++) {
+     //iterate through columns
+     //columns would be accessed using the "col" variable assigned in the for loop
+     col.textContent="-";
+   }
+}
 
 function WriteBoard() {
   console.log("WriteBoard() function reached");
@@ -17,8 +30,28 @@ function WriteBoard() {
     player.setAttribute("style", "color: red; font-weight: bold;");
   }
 
+  //Count open slots everytime use clicks
 
-  //  MainGameLoop();
+  var slotsLeft=0;
+   for (var i = 0, row; row = table.rows[i]; i++) {
+      //iterate through rows
+      //rows would be accessed using the "row" variable assigned in the for loop
+      for (var j = 0, col; col = row.cells[j]; j++) {
+        //iterate through columns
+        //columns would be accessed using the "col" variable assigned in the for loop
+        if(col.textContent === "-")
+        {
+          slotsLeft++;
+        }
+      }
+   }
+  if(slotsLeft === 0)
+  {
+    shouldQuit=true;
+    DisplayModal();
+  }
+
+  // MainGameLoop();
 }
 
 
@@ -44,9 +77,55 @@ else
 }
 
 
-
  WriteBoard();
 }
 
 
-function MainGameLoop() {}
+function MainGameLoop()
+{
+
+
+
+
+
+}
+
+
+//Display Modal
+function DisplayModal()
+{
+  let modal = document.getElementById("modal-holder");
+  modal.style.display="block";
+}
+
+//Code for closebutton
+function close()
+{
+  var modal = document.getElementById("modal-holder");
+  modal.style.display="none";
+}
+
+//If the user clicks outside of modal-holders content divs
+window.onclick = function(event) {
+  if (event.target.id === "modal-holder") {
+    document.getElementById("modal-holder").style.display = "none";
+
+  }
+}
+
+function ResetBoard()
+{
+  console.log("reset board");
+  var table = document.getElementById("table");
+
+
+  for (var i = 0, row; row = table.rows[i]; i++) {
+     //iterate through rows
+     //rows would be accessed using the "row" variable assigned in the for loop
+     for (var j = 0, col; col = row.cells[j]; j++) {
+       //iterate through columns
+       //columns would be accessed using the "col" variable assigned in the for loop
+       col.textContent="-";
+     }
+  }
+}
