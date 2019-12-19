c1e3bdeb6fbc014321a711130b8ae32c1c02616f
Kevin Engström <kevin.engstrom@hotmail.com>
2019-12-19 08:41:33 +0100

    Add 3 different arrays for each row of table

--- TicTacToe.js
+++ TicTacToe.js
@@ -5,198 +5,133 @@ modalClose.addEventListener("click", close);
 document.getElementById("table").addEventListener("click", makeMove);
 
 var mappen = new Array(3).fill(new Array(3)).fill("");
+var firstRow = new Array(3);
+var secondRow = new Array(3);
+var thirdRow = new Array(3);
 
 var table = document.getElementById("table");
 
-
-for (var i = 0, row; row = table.rows[i]; i++) {
-   //iterate through rows
-   //rows would be accessed using the "row" variable assigned in the for loop
-   for (var j = 0, col; col = row.cells[j]; j++) {
-     //iterate through columns
-     //columns would be accessed using the "col" variable assigned in the for loop
-     col.textContent="-";
-   }
+for (var i = 0, row; (row = table.rows[i]); i++) {
+  //iterate through rows
+  //rows would be accessed using the "row" variable assigned in the for loop
+  for (var j = 0, col; (col = row.cells[j]); j++) {
+    //iterate through columns
+    //columns would be accessed using the "col" variable assigned in the for loop
+    col.textContent = "-";
+  }
 }
 
 function WriteBoard() {
-  if(!pause) {
-  console.log("WriteBoard() function reached");
-  var player = document.getElementById("current-player");
+  if (!pause) {
+    console.log("WriteBoard() function reached");
+    var player = document.getElementById("current-player");
 
+    if (player.textContent === "Blue") {
+      player.setAttribute("style", "color: blue; font-weight: bold;");
+    } else {
+      player.setAttribute("style", "color: red; font-weight: bold;");
+    }
 
-  if (player.textContent === "Blue") {
-    player.setAttribute("style", "color: blue; font-weight: bold;");
-  } else {
-    player.setAttribute("style", "color: red; font-weight: bold;");
-  }
+    //Count open slots everytime use clicks
 
-  //Count open slots everytime use clicks
-
-  var slotsLeft=0;
-   for (var i = 0, row; row = table.rows[i]; i++) {
+    var slotsLeft = 0;
+    for (var i = 0, row; (row = table.rows[i]); i++) {
       //iterate through rows
       //rows would be accessed using the "row" variable assigned in the for loop
-      for (var j = 0, col; col = row.cells[j]; j++) {
+      for (var j = 0, col; (col = row.cells[j]); j++) {
         //iterate through columns
         //columns would be accessed using the "col" variable assigned in the for loop
-        if(col.textContent === "-")
-        {
+        if (col.textContent === "-") {
           slotsLeft++;
         }
       }
-   }
-  if(slotsLeft === 0)
-  {
-    shouldQuit=true;
-    DisplayModal("Its a TIE!");
-  }
-  CheckWinner();
+    }
+    if (slotsLeft === 0) {
+      shouldQuit = true;
+      DisplayModal("Its a TIE!");
+    }
+    CheckWinner();
 
-  // MainGameLoop();
+    // MainGameLoop();
   }
 }
 
-
-
-function makeMove(event)
-{
-    var player = document.getElementById("current-player");
+function makeMove(event) {
+  var player = document.getElementById("current-player");
   console.log("click");
 
- let sign = "";
+  let sign = "";
 
+  if (event.target.textContent === "X" || event.target.textContent === "O") {
+    //Do nothing, since the cell was already taken by any player
+  } else {
+    player.textContent === "Blue" ? (sign = "X") : (sign = "O");
+    player.textContent === "Blue"
+      ? (player.textContent = "Red")
+      : (player.textContent = "Blue");
+    event.target.textContent = sign;
+  }
 
-
-if(event.target.textContent === "X" || event.target.textContent === "O")
-{
-  //Do nothing, since the cell was already taken by any player
-}
-else
-{
-  player.textContent === "Blue" ? sign = "X" : sign = "O";
- player.textContent === "Blue" ? player.textContent = "Red" : player.textContent = "Blue";
- event.target.textContent = sign;
-}
-
-
- WriteBoard();
+  WriteBoard();
 }
 
-
-function MainGameLoop()
-{
-
-
-
-
-
-}
-
+function MainGameLoop() {}
 
 //Display Modal
-function DisplayModal(s)
-{
+function DisplayModal(s) {
   let modal = document.getElementById("modal-holder");
-  document.getElementById("modal-content").textContent=s;
-  modal.style.display="block";
+  document.getElementById("modal-content").textContent = s;
+  modal.style.display = "block";
+  pause = true;
 }
 
 //Code for closebutton
-function close()
-{
+function close() {
   var modal = document.getElementById("modal-holder");
-  modal.style.display="none";
+  modal.style.display = "none";
 }
 
 //If the user clicks outside of modal-holders content divs
 window.onclick = function(event) {
   if (event.target.id === "modal-holder") {
     document.getElementById("modal-holder").style.display = "none";
-
   }
-}
+};
 
-function ResetBoard()
-{
+function ResetBoard() {
   console.log("reset board");
   var table = document.getElementById("table");
 
-
-  for (var i = 0, row; row = table.rows[i]; i++) {
-     //iterate through rows
-     //rows would be accessed using the "row" variable assigned in the for loop
-     for (var j = 0, col; col = row.cells[j]; j++) {
-       //iterate through columns
-       //columns would be accessed using the "col" variable assigned in the for loop
-       col.textContent="-";
-     }
+  for (var i = 0, row; (row = table.rows[i]); i++) {
+    //iterate through rows
+    //rows would be accessed using the "row" variable assigned in the for loop
+    for (var j = 0, col; (col = row.cells[j]); j++) {
+      //iterate through columns
+      //columns would be accessed using the "col" variable assigned in the for loop
+      col.textContent = "-";
+    }
   }
 }
 
-function CheckWinner()
-{
+function CheckWinner() {
   console.log("checking winner");
   var table = document.getElementById("table");
 
-
-  for (var i = 0, row; row = table.rows[i]; i++) {
-     //iterate through rows
-     //rows would be accessed using the "row" variable assigned in the for loop
-     for (var j = 0, col; col = row.cells[j]; j++) {
-       //iterate through columns
-       //columns would be accessed using the "col" variable assigned in the for loop
-       if(row.cells[0].textContent === "X" && row.cells[1].textContent === "X" && row.cells[2].textContent === "X")
-       {
-         DisplayModal("Blue Wins!");
-         pause = true;
-       }
-       if(row.cells[0].textContent === "O" && row.cells[1].textContent === "O" && row.cells[2].textContent === "O")
-       {
-         DisplayModal("Red Wins!");
-         pause = true;
-       }
-       if(row.cells[0].textContent === "X" && row.cells[4].textContent === "X" && row.cells[8].textContent === "X")
-       {
-         DisplayModal("Blue Wins!");
-         pause = true;
-       }
-        if(row.cells[0].textContent === "O" && row.cells[4].textContent === "O" && row.cells[8].textContent === "O")
-       {
-         DisplayModal("Red Wins!");
-         pause = true;
-       }
-        if(row.cells[3].textContent === "X" && row.cells[4].textContent === "X" && row.cells[5].textContent === "X")
-       {
-         DisplayModal("Blue Wins!");
-         pause = true;
-       }
-       if(row.cells[3].textContent === "O" && row.cells[4].textContent === "O" && row.cells[5].textContent === "O")
-       {
-         DisplayModal("Red Wins!");
-         pause = true;
-       }
-       if(row.cells[6].textContent === "X" && row.cells[7].textContent === "X" && row.cells[8].textContent === "X")
-       {
-         DisplayModal("Blue Wins!");
-         pause = true;
-       }
-      if(row.cells[6].textContent === "O" && row.cells[7].textContent === "O" && row.cells[8].textContent === "O")
-       {
-         DisplayModal("Red Wins!");
-         pause = true;
-       }
-       if(row.cells[6].textContent === "X" && row.cells[4].textContent === "X" && row.cells[2].textContent === "X")
-       {
-         DisplayModal("Blue Wins!");
-         pause = true;
-       }
-       if(row.cells[6].textContent === "O" && row.cells[4].textContent === "O" && row.cells[2].textContent === "O")
-       {
-         DisplayModal("Red Wins!");
-         pause = true;
-
-     }
+  for (var i = 0, row; (row = table.rows[i]); i++) {
+    //iterate through rows
+    //rows would be accessed using the "row" variable assigned in the for loop
+    for (var j = 0, col; (col = row.cells[j]); j++) {
+      //iterate through columns
+      //columns would be accessed using the "col" variable assigned in the for loop
+      if (i === 0) {
+        firstRow[j] = col;
+      }
+      if (i === 1) {
+        secondRow[j] = col;
+      }
+      if (i === 2) {
+        thirdRow[j] = col;
+      }
+    }
   }
-}
 }
